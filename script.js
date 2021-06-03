f59fa68d440f2879e82c19c0bb58c480ecf1128f
Siméon TABASIMWA <63101571+Simeon-TabaK@users.noreply.github.com>
2021-06-03 12:11:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('button').addEventListener('click',
+function(){
+    document.querySelector('.modal-bg').style.display='flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.modal-bg').style.display='none';
+});
